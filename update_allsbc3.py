68c62f606c3bb4798541929b1a4c728e4fe582a6
Ignacio Toledo <itoledoc@gmail.com>
2015-12-13 13:51:11 -0300

    update script for EPT

--- update_allsbc3.py
+++ update_allsbc3.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 __author__ = 'ignacio'
 
-import WtoDataBase3 as wto
+import DsaDataBase3 as wto
 import pandas as pd
 
-datas = wto.WtoDatabase3(path='/users/aod/.cycle3pt/', loadp1=False)
+datas = wto.DsaDatabase3(path='/users/aod/.cycle3pt/', loadp1=False)
 allsbC3 = pd.merge(
     pd.merge(
         datas.projects.query('CYCLE not in ["2013.1", "2013.A"]'), 
         datas.schedblocks, on='OBSPROJECT_UID'), 
     datas.sb_status, 
-    on='SB_UID', how='left', suffixes=["","_SB"])
+    on='SB_UID', how='left', suffixes=["", "_SB"])
 
 allsbC3 = allsbC3.query('SB_STATE not in ["Canceled", "Deleted"]')[
     ['CODE', 'OBSPROJECT_UID', 'sbName', 'SB_UID', 'band', 'repfreq', 
      'array', 'EXEC', 'RA', 'DEC', 'BestConf']]
-allsbC3.sort('CODE').to_csv('/data/Cycle3/daily-backup/allC3.sbinfo', sep='\t', index=False, header=False)
+allsbC3.sort('CODE').to_csv(
+        '/data/Cycle3/daily-backup/allC3.sbinfo', sep='\t', index=False,
+        header=False)
 datas._cursor.close()
 datas._connection.close()
-

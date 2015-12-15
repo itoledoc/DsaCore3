6688270ce2d5bb12a74ef712159883c7c1f3bc56
Ignacio Toledo <itoledoc@gmail.com>
2015-12-15 18:28:13 -0300

    fix small bug

--- update_mwto.py
+++ update_mwto.py
@@ -36,7 +36,7 @@ dsa.selection_df['PWV now date'] = (
     pd.read_sql('pwv_data', engine).date.values[0] + ' ' +
     pd.read_sql('pwv_data', engine).time.values[0])
 dsa.selection_df['date'] = str(dsa._ALMA_ephem.date)
-dsa.selection_df['arrayname'] = dsa.bl_arrays.iloc[0, 3]
+dsa.selection_df['arrayname'] = dsa.arrays.iloc[0, 3]
 scorer = dsa.master_dsa_df.apply(
     lambda x: WtoScor.calc_all_scores(
         pwv, x['maxPWVC'], x['Exec. Frac'], x['sbName'], x['array'], x['ARcor'],
@@ -51,15 +51,19 @@ dsa.master_dsa_df['allconfs'] = dsa.obs_param.apply(
 
 scorer.to_sql('scorer_wto_test', engine, index_label='SBUID',
               if_exists='replace', schema='wto')
+print('scorer written')
 dsa.inputs.to_sql('inputs_wto_text', engine, index_label='Cycle',
                   if_exists='replace', schema='wto')
+print('inputs written')
 dsa.selection_df.to_sql('selection_wto_test', engine, index_label='SBUID',
                         if_exists='replace', schema='wto')
+print('selection written')
 dsa.master_dsa_df.to_sql('master_wto_test', engine, index_label='SBUID',
                          if_exists='replace', schema='wto')
+print('master written')
 dsa.obs_param.to_sql('staticparam_wto_test', engine, index_label='SBUID',
                      if_exists='replace', schema='wto')
-
+print('stat param written')
 # C36-1 default
 
 dsa.selector(
@@ -85,14 +89,20 @@ dsa.master_dsa_df['allconfs'] = dsa.obs_param.apply(
 
 scorer.to_sql('scorer_wto_c361', engine, index_label='SBUID',
               if_exists='replace', schema='wto')
+print('scorer written')
 dsa.inputs.to_sql('inputs_wto_c361', engine, index_label='Cycle',
                   if_exists='replace', schema='wto')
+print('inputs written')
 dsa.selection_df.to_sql('selection_wto_c361', engine, index_label='SBUID',
                         if_exists='replace', schema='wto')
+print('selection written')
 dsa.master_dsa_df.to_sql('master_wto_c361', engine, index_label='SBUID',
                          if_exists='replace', schema='wto')
+print('master written')
 dsa.obs_param.to_sql('staticparam_wto_c361', engine, index_label='SBUID',
                      if_exists='replace', schema='wto')
+print('stat param written')
+
 
 datas._cursor.close()
 datas._connection.close()

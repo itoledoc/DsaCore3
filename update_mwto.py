14bd99bd5c9dd372e6278a21336a169c350c68ec
Ignacio Toledo <itoledoc@gmail.com>
2015-12-17 17:52:16 -0300

    update to use new postgresql

--- update_mwto.py
+++ update_mwto.py
@@ -12,7 +12,7 @@ from astropy.utils import iers
 iers.IERS.iers_table = iers.IERS_A.open(
     download_file(iers.IERS_A_URL, cache=True))
 
-engine = create_engine('postgresql://wto:wto2020@dmg02.sco.alma.cl:5432/aidadb')
+engine = create_engine('postgresql://dsacore:dsa2020@tableau.alma.cl:5432/dsa_data')
 refr = False
 if time.time() - os.path.getmtime('/users/aod/.mwto/') > 3600.:
     refr = True
@@ -50,19 +50,19 @@ dsa.master_dsa_df['allconfs'] = dsa.obs_param.apply(
          str(x['C36_5']), str(x['C36_7']), str(x['C36_8'])]), axis=1)
 
 scorer.to_sql('scorer_wto_test', engine, index_label='SBUID',
-              if_exists='replace', schema='wto')
+              if_exists='replace')
 print('scorer written')
 dsa.inputs.to_sql('inputs_wto_text', engine, index_label='Cycle',
-                  if_exists='replace', schema='wto')
+                  if_exists='replace')
 print('inputs written')
 dsa.selection_df.to_sql('selection_wto_test', engine, index_label='SBUID',
-                        if_exists='replace', schema='wto')
+                        if_exists='replace')
 print('selection written')
 dsa.master_dsa_df.to_sql('master_wto_test', engine, index_label='SBUID',
-                         if_exists='replace', schema='wto')
+                         if_exists='replace')
 print('master written')
 dsa.obs_param.to_sql('staticparam_wto_test', engine, index_label='SBUID',
-                     if_exists='replace', schema='wto')
+                     if_exists='replace')
 print('stat param written')
 # C36-1 default
 
@@ -88,19 +88,19 @@ dsa.master_dsa_df['allconfs'] = dsa.obs_param.apply(
          str(x['C36_5']), str(x['C36_7']), str(x['C36_8'])]), axis=1)
 
 scorer.to_sql('scorer_wto_c361', engine, index_label='SBUID',
-              if_exists='replace', schema='wto')
+              if_exists='replace')
 print('scorer written')
 dsa.inputs.to_sql('inputs_wto_c361', engine, index_label='Cycle',
-                  if_exists='replace', schema='wto')
+                  if_exists='replace')
 print('inputs written')
 dsa.selection_df.to_sql('selection_wto_c361', engine, index_label='SBUID',
-                        if_exists='replace', schema='wto')
+                        if_exists='replace')
 print('selection written')
 dsa.master_dsa_df.to_sql('master_wto_c361', engine, index_label='SBUID',
-                         if_exists='replace', schema='wto')
+                         if_exists='replace')
 print('master written')
 dsa.obs_param.to_sql('staticparam_wto_c361', engine, index_label='SBUID',
-                     if_exists='replace', schema='wto')
+                     if_exists='replace')
 print('stat param written')
 
 

01f8001a893ac183d09008bdf224b3fecb600429
javarias <jorge.avarias@gmail.com>
2015-12-17 18:09:54 +0000

    Improved a little the code used by synchronization lock

--- serv.py
+++ serv.py
@@ -3,6 +3,8 @@ import DsaDataBase3 as Data
 import DsaAlgorithm3 as Dsa
 import DsaScorers3 as DsaScore
 import pandas as pd
+import threading
+import time
 
 from twisted.web import xmlrpc, server
 from astropy.utils.data import download_file
@@ -10,11 +12,8 @@ from astropy.utils import iers
 from sqlalchemy import create_engine
 engine = create_engine('postgresql://wto:wto2020@dmg02.sco.alma.cl:5432/aidadb')
 
-import threading
-import time
 
-
-class __refreshingThread(threading.Thread):
+class RefreshThread(threading.Thread):
 
     def __init__(self, dsa_core_instance, sleep_time=120):
         """
@@ -29,13 +28,17 @@ class __refreshingThread(threading.Thread):
     def run(self):
         print "Starting refreshing thread"
         while not self.stop:
-            print "Staring refresh..."
-            data = Data.DsaDatabase3(refresh_apdm=True, allc2=False, loadp1=False)
-            self.__dsa.data_lock.acquire()
-            self.__dsa.data = data
-            self.__dsa.data.update_status()
-            self.__dsa.data_lock.release()
-            print "Refresh done. Waiting", self.__wait_time, "seconds until next refresh"
+            try:
+                print "Staring refresh..."
+                data = Data.DsaDatabase3(refresh_apdm=True, allc2=False, loadp1=False)
+                with self.__dsa.lock:
+                    self.__dsa.data = data
+                    self.__dsa.data.update_status()
+                print "Refresh done. Waiting", self.__wait_time, "seconds until next refresh"
+            except Exception as ex:
+                print "Problem refreshing the data. Cause:", ex
+                print "Waiting", self.__wait_time, "seconds until next refresh"
+
             time.sleep(self.__wait_time)
 
 
@@ -51,10 +54,9 @@ class DSACoreService(xmlrpc.XMLRPC):
 
         iers.IERS.iers_table = iers.IERS_A.open(
             download_file(iers.IERS_A_URL, cache=True))
-        self.data_lock = threading.Lock()
-        self.data_lock.acquire()
-        self.data = Data.DsaDatabase3(refresh_apdm=True, allc2=False, loadp1=False)
-        self.data_lock.release()
+        self.lock = threading.Lock()
+        with self.lock:
+            self.data = Data.DsaDatabase3(refresh_apdm=True, allc2=False, loadp1=False)
 
     def xmlrpc_run(self,
                    array_kind='TWELVE-M',
@@ -71,9 +73,8 @@ class DSACoreService(xmlrpc.XMLRPC):
                    pwv=0.5,
                    timestring=''):
 
-        self.data_lock.acquire()
-        dsa = Dsa.DsaAlgorithm3(self.data)
-        self.data_lock.release()
+        with self.lock:
+            dsa = Dsa.DsaAlgorithm3(self.data)
 
         if conf == '' or array_kind != 'TWELVE-M':
             conf = None
@@ -125,14 +126,16 @@ class DSACoreService(xmlrpc.XMLRPC):
         :param array_id:
         :return:
         '''
-        dsa = Dsa.DsaAlgorithm3(self.data)
+        with self.lock:
+            dsa = Dsa.DsaAlgorithm3(self.data)
         dsa._query_array()
         a = dsa._get_bl_prop(array_id)
 
         return float(a[0])
 
     def xmlrpc_get_arrays(self, array_kind):
-        dsa = Dsa.DsaAlgorithm3(self.data)
+        with self.lock:
+            dsa = Dsa.DsaAlgorithm3(self.data)
         dsa._query_array(array_kind=array_kind)
         if dsa.arrays is None:
             return 'No Arrays'
@@ -140,13 +143,13 @@ class DSACoreService(xmlrpc.XMLRPC):
         return dsa.arrays.SE_ARRAYNAME.unique().tolist()
 
     def xmlrpc_update_data(self):
-
-        self.data = Data.DsaDatabase3(
-                refresh_apdm=True, allc2=False, loadp1=False)
+        with self.lock:
+            self.data = Data.DsaDatabase3(
+                    refresh_apdm=True, allc2=False, loadp1=False)
 
     def xlmrpc_update_apdm(self, obsproject_uid):
-
-        self.data._update_apdm(obsproject_uid)
+        with self.lock:
+            self.data._update_apdm(obsproject_uid)
 
     def xmlrpc_run_full(self,
                    array_kind='TWELVE-M',
@@ -163,9 +166,9 @@ class DSACoreService(xmlrpc.XMLRPC):
                    pwv=0.5,
                    timestring=''):
 
-        self.data_lock.acquire()
+        self.lock.acquire()
         dsa = Dsa.DsaAlgorithm3(self.data)
-        self.data_lock.release()
+        self.lock.release()
 
         if conf == '' or array_kind != 'TWELVE-M':
             conf = None
@@ -220,7 +223,8 @@ class DSACoreService(xmlrpc.XMLRPC):
 if __name__ == '__main__':
     from twisted.internet import reactor
     r = DSACoreService()
-    thread = __refreshingThread(r)
+    thread = RefreshThread(r)
+    thread.daemon = True
     thread.start()
     reactor.listenTCP(7080, server.Site(r))
     reactor.run()
